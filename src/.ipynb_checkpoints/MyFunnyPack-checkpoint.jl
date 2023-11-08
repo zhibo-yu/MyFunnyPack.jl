43bb5a1acb16ee5620163fb4bbe6ef0f6de6e89d
Zhibo Yu <zvy5225@p-gc-3111.2e.hpc.psu.edu>
2023-11-08 14:57:56 -0500

    My funny package code

--- src/.ipynb_checkpoints/MyFunnyPack-checkpoint.jl
+++ src/.ipynb_checkpoints/MyFunnyPack-checkpoint.jl
@@ -1,5 +1,17 @@
 module MyFunnyPack
 
-greet() = print("Hello World!")
+using Distributions
+
+export pdf_normal
+
+"""
+   pdf_normal(x, mu, sigma)
+
+Return pdf of a normal distribution with mean mu and standard deviation sigma at point x.
+"""
+function pdf_normal(x::Number, mu::Number, sigma::Number)
+  dist = Normal(mu,sigma)
+  pdf(dist,x)
+end
 
 end # module MyFunnyPack
